0b9df54d3928107408d1ef330868e67e95ab8998
AlessandroCom <atkdef8@gmail.com>
2021-01-05 00:38:01 +0000

    Indexes not really working...

--- main.py
+++ main.py
@@ -1,13 +1,34 @@
 from random import randint, sample
 
-def env_init():
-  board_val = (randint(1,3)*2, randint(3,5))
-  print(board_val)
-  max_board = max(board_val[0], board_val[1])
-  half_board = int(board_val[0]*board_val[1]/2) 
-  points_num = randint(max_board, half_board)
-  points_val = set(sample(range(2*half_board), points_num))
-  return points_val
-
-
-print(env_init()) 
+class Environment:
+  
+  def __init__(self):
+    
+    rows = randint(1,2)*3
+    cols = randint(3,6)
+    self.board = (rows, cols, rows*cols)
+    print("{} rows * {} cols = {} cells".format(*self.board))
+    self.points = sample(range(self.board[2]), randint(self.board[1], int(self.board[2]/3)))
+    print("Notable points in cells: {}".format(sorted(self.points)))
+    
+  def __repr__(self):
+    
+    i_idx = self.points.pop(0)
+    print(i_idx)
+    g_idx = self.points.pop(-1)
+    print(g_idx)
+    e_list = ["o" if i in self.points else "i" if i == i_idx else "g" if i == g_idx else "#" for i in range(self.board[2])]
+    return e_list
+    
+  def __str__(self):
+    
+    e_list = self.__repr__()
+    e_matr = [[e_list[j+i*self.board[1]] for j in range(self.board[1])] for i in range(self.board[0])]
+    e_str = ""
+    for i in e_matr:
+      e_str = str(*i) + "/n"
+    return e_str
+    
+Lalla = Environment()
+print(*Lalla.__repr__())
+print(Lalla)
